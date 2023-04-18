955e1066c88ef081c51db3bf653e05217697c5e6
Jty98 <clclcfnfn@naver.com>
2023-04-18 11:00:50 +0900

    hi

--- OOP_01/src/com/javalec/base/Main.java
+++ OOP_01/src/com/javalec/base/Main.java
@@ -1,17 +1,36 @@
 package com.javalec.base;
 
+import java.util.Scanner;
+
 import com.javalec.function.SumEvenOdd;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// 1~10까지의 합계를 구하고 그 합이 짝수인지 홀수인지 판별(class 활용)
+
+		// ----------------
+		// 사용자로 부터 숫자 2개를 입력받아 범위의 합계를 구하고 짝수 홀수 판단.
+		// ----------------
+		Scanner scanner = new Scanner(System.in);
+		int startNum = 0;
+		int endNum = 0;
+		int total = 0; // 합계
+		String totalEvenOdd = ""; // total의 짝홀수 판별
 		
+		System.out.print("첫번째 숫자를 입력 하세요");
+		startNum = scanner.nextInt();
+		System.out.print("두번째 숫자를 입력 하세요");
+		endNum = scanner.nextInt();
+
 		SumEvenOdd sumEvenOdd = new SumEvenOdd();
-		int result = sumEvenOdd.sumCalc(1, 10);
-		System.out.println("1부터 10까지의 합계는 " + result);
-		result = sumEvenOdd.sumCalc(10, 1000);
-		System.out.println("100부터 1000까지의 합계는 " + result);
+		total = sumEvenOdd.sumCalc(startNum, endNum); // return 받는곳
+		System.out.println("1부터 10까지의 합계는 " + total);
+//		total = sumEvenOdd.sumCalc(10, 1000);
+//		System.out.println("100부터 1000까지의 합계는 " + total);
+
+		totalEvenOdd = sumEvenOdd.evenOdd(total);
+		System.out.println("1부터 10까지의 합계는 " + totalEvenOdd);
+
 	}
 
 }
--- OOP_01/src/com/javalec/function/SumEvenOdd.java
+++ OOP_01/src/com/javalec/function/SumEvenOdd.java
@@ -1,26 +1,41 @@
 package com.javalec.function;
 
+import java.util.Scanner;
+
 public class SumEvenOdd {
 	
 	// Field, Property
 	
 	// Constructor
-	public SumEvenOdd() {
-		// TODO Auto-generated constructor stub
+	public SumEvenOdd() {								// new들어가는 것과 헷갈리지않기
+		// TODO Auto-generated constructor stub			// empty : 비어있는 constructor
 	}
 	
 	// Method
 	// 합계를 구한다.
-	public int sumCalc(int startNum, int endNum) {		// 메인으로 넘겨주기
+	public int sumCalc(int startNum, int endNum) {		// 메인으로 넘겨주기, return되는 변수 타입에 따라 int, String 다름
+		Scanner scanner = new Scanner(System.in);
+		
 		int sum = 0;
 		for(int i=startNum; i<=endNum; i++) {
 			sum += i;
 		}
-		return sum;
-		
+		return sum;	
 	}
 	
 	// 짝수 홀수 판단.
+	public String evenOdd(int sum) {					// (파라미터)
+		String result = "";
+		if(sum % 2 ==0) {
+			result = "짝수 입니다";
+		}else {
+			result = "홀수 입니다";
+		}
+		return result;
+		
+														// *떠있고 끝나면 저장하기
+		
+	}
 	
 
 }
